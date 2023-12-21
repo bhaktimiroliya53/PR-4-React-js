13162bd399b615030d37fe56490241fe9e3b3e27
Parth Miroliya <parthmiroliya@gmail.com>
2023-12-21 23:35:09 +0530

    PR-4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [input, setInput] = useState([{
+    name: '', phone: '',
+  }])
+
+  const handleChange = () => {
+    let newRcord = {name : '',phone : ''};
+    let add = [...input,newRcord];
+    setInput(add);
+  }
+
+  const remove = (index) => {
+    let old = [...input];
+    old.splice(index,1);
+    setInput(old)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <h1>Dynamic Form</h1>
+      <table border={1}>
+        <tbody>
+          {
+            input.map((value, index) => {
+              if(index == 0){
+                return (
+                  <tr key={index}>
+                    <td style={{margin : "5px 10px" , fontSize : "18px"}}>Full Name</td>
+                    <td><input type='text' style={{margin : "5px 10px" ,fontSize : "18px"}}/></td>
+                    <td style={{margin : "5px 10px" , fontSize : "18px"}}>Email Address</td>
+                    <td><input type='text' style={{margin : "5px 10px" ,fontSize : "18px"}}/></td>
+                    <td style={{margin : "5px 10px" , fontSize : "18px"}}>Salary</td>
+                    <td><input type='text' style={{margin : "5px 10px" ,fontSize : "18px"}}/></td>
+                    <button onClick={() => handleChange()} style={{margin : "5px 10px" , fontSize : "18px"}}>Add</button>
+                  </tr>
+                )
+              }else{
+                return (
+                  <tr key={index}>
+                    <td style={{margin : "5px 10px" , fontSize : "18px"}}>Full Name</td>
+                    <td><input type='text' style={{margin : "5px 10px" , fontSize : "18px"}}/></td>
+                    <td style={{margin : "5px 10px" , fontSize : "18px"}}>Email Address</td>
+                    <td><input type='text' style={{margin : "5px 10px" , fontSize : "18px"}}/></td>
+                    <td style={{margin : "5px 10px" , fontSize : "18px"}}>Salary</td>
+                    <td><input type='text' style={{margin : "5px 10px" , fontSize : "18px"}}/></td>
+                    <td>
+                      <button onClick={ () => remove(index)} style={{fontSize : "18px"}}>remove</button>
+                    </td>
+                  </tr>
+                )
+              }
+            })
+          }
+        </tbody>
+      </table>
+    </center>
   );
 }
-
 export default App;
